79540e45681468c259def39ac06fe9903f4631ed
Ezhko <ucsd.camp.21@gmail.com>
2021-09-22 19:59:32 -0400

    add application prompts

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,6 +4,36 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   // TODO: Your code to generate a password goes here. Be sure to return a password
   // string. You will need to change the following two lines of code.
+
+  // ask user for length of password
+  var passLength = prompt("Please enter a number between 8 and 128 to specify password length.");
+
+  //validate length input from user
+
+  if (passLength > 8 && passLength < 128) {
+
+    //if use input is correct, continue with prompts:
+    
+    // choose lower case
+    var useLowerCase = confirm("Would you like to use lower case letters?");
+
+    // choose upper case
+    var useUpperCase = confirm("Would you like to use Upper Case letters?");
+    
+    // choose numbers
+    var useNumbers = confirm("Would you like to use numbers?");
+    
+    // choose special
+    var useSpecial = confirm ("Would you like to use special characters?");  
+    
+    // if user input is false, then alert and prompt to do again:
+  } else {
+   alert ("Your password must be between 8 and 128 characters.")
+   return generatePassword();
+  }
+  
+  // 
+  
   var password = "password1234";
   return password;
 }
